d261d87af394aa25c2e66b14f90ab1f33cf337d2
unknown <ivanclake.ivanclake@mail.ru>
2025-09-22 23:38:54 +0500

    sss

--- tasks/task3.py
+++ tasks/task3.py
@@ -4,9 +4,10 @@
 
 def solve():
 # Ниже пишите решение задачии(Обязательно поставьте четыре пробела после функции Solve())
-
-   
-
+    weight = float(input("Введите число для значения weight: "))
+    height = float(input("Введите число для height: "))
+    bmi = weight / height ** 2
+    print("Индекс: ", bmi)
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
